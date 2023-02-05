9e03fca6c3770f649424c15805988fa10202a82e
PavelErichev <dustfud@yandex.ru>
2023-02-05 20:06:28 +0300

    Commit_1.1

---
+++ src/test/java/ru/kata/spring/boot/rest/Spring_Rest_3_1_5/SpringRest315ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.kata.spring.boot.rest.Spring_Rest_3_1_5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRest315ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
