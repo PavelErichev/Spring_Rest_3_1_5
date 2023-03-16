8adc50c58b1c8be624e6d8aa7c67621b490bbcf1
PavelErichev <dustfud@yandex.ru>
2023-03-16 23:23:14 +0300

    Commit_1.4

--- src/main/java/ru/kata/spring/boot/rest/Spring_Rest_3_1_5/controller/AdminViewController.java
+++ src/main/java/ru/kata/spring/boot/rest/Spring_Rest_3_1_5/controller/AdminViewController.java
@@ -1,17 +1,20 @@
 package ru.kata.spring.boot.rest.Spring_Rest_3_1_5.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import ru.kata.spring.boot.rest.Spring_Rest_3_1_5.entity.User;
 
 @Controller
 public class AdminViewController {
 
     @GetMapping("/admin")
-    public String showAdminPage(){
-        return "admin";
+    public String showAdminPage(Model model) {
+        model.addAttribute("newUser", new User());
+        return "admin1";
     }
 
-    @GetMapping("/admin/user-create")
+    /*@GetMapping("/admin/user-create")
     public String newUser(){
         return "user-create";
     }
@@ -19,6 +22,6 @@ public class AdminViewController {
     @GetMapping("/userAdmin")
     public String getUserAdmin() {
         return "userAdmin";
-    }
+    }*/
 
 }
--- src/main/java/ru/kata/spring/boot/rest/Spring_Rest_3_1_5/controller/UserViewController.java
+++ src/main/java/ru/kata/spring/boot/rest/Spring_Rest_3_1_5/controller/UserViewController.java
@@ -10,7 +10,7 @@ public class UserViewController {
 
     @GetMapping
     public String userInfo() {
-        return "user";
+        return "user1";
     }
 
 }
